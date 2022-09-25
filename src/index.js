589d681af39ff8fd25ddf8d81e1a043083162e2b
tfarnham <1356212+tfarnham@users.noreply.github.com>
2022-09-24 20:55:27 -0700

    Initial commit
    First progress getting React v6 to render the HashRouter. Bootstrap imported.

--- src/index.js
+++ src/index.js
@@ -3,15 +3,53 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, Routes, Link, HashRouter} from 'react-router-dom';
+import NavBar from './navbar';
+import Home from './home';
+import CreateAccount from './createaccount';
+import Deposit from './deposit';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+export const UserContext = React.createContext(null);
+//const Route       = ReactRouterDOM.Route;
+//const Link        = ReactRouterDOM.Link;
+//const HashRouter  = ReactRouterDOM.HashRouter;
+
+function Spa() {
+  return (
+    <HashRouter>
+      <NavBar></NavBar>
+      <UserContext.Provider value={{users:[{name:'abel',email:'abel@mit.edu',password:'secret',balance:100}]}}>
+      <div className="container" style={{padding: "20px"}}>TEST2
+        </div>
+      <Routes>
+
+        <Route path="/" exact element={<Home />} />
+          <Route path="/CreateAccount/" element={<CreateAccount />} />
+          <Route path="/Deposit/" element={<Deposit />} />
+          </Routes>
+          TEST
+
+      </UserContext.Provider>      
+    </HashRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Spa />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/*
+          <Route path="/" exact component={Home} />
+          <Route path="/CreateAccount/" component={CreateAccount} />
+          <Route path="/login/" component={Login} />
+          <Route path="/deposit/" component={Deposit} />
+          <Route path="/withdraw/" component={Withdraw} />
+          <Route path="/balance/" component={Balance} />
+          <Route path="/alldata/" component={AllData} />
+*/
